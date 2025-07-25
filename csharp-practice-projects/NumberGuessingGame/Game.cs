3e400c06f2ef12b57a8f4a54898f2b0c91025821
Charaka Gunasinghe <charaka.info@gmail.com>
2025-07-25 14:21:02 +0530

    number guessing game implemented

---
+++ csharp-practice-projects/NumberGuessingGame/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class Game
+    {
+        private int highScore = int.MaxValue;
+
+        public void Start()
+        {
+            bool playAgain = true;
+            while(playAgain)
+            {
+                Difficulty dificulty = GetDificulty();
+
+                Round round = new Round(dificulty);
+                round.Play();
+
+                if (round.Attempts < this.highScore)
+                {
+                    this.highScore = round.Attempts;
+                    Console.WriteLine($"\nNew highscore: {this.highScore} attempts");
+                }
+                else
+                {
+                    Console.WriteLine($"\nYour score: {round.Attempts}");
+                    Console.WriteLine($"Current highscore: {this.highScore} attempts");                    
+                }
+
+                Console.Write("\nplay again (y/n): ");
+                string confrim = Console.ReadLine().Trim().ToLower();
+
+                playAgain = confrim == "y";
+
+                if (playAgain)
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("------- Thank you for playing -------");
+                }
+               
+            }
+        }
+
+        private Difficulty GetDificulty()
+        {
+            Console.WriteLine("------- Number Guessing Game -------");
+            Console.WriteLine("\nSelect dificulty: ");
+            Console.WriteLine($"1 - {Difficulty.Easy} (1 to 10)");
+            Console.WriteLine($"2 - {Difficulty.Medium} (1 to 50)");
+            Console.WriteLine($"3 - {Difficulty.Hard} (1 to 100)");
+
+            while(true)
+            {
+                Console.Write("\nyour choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        return Difficulty.Easy;
+                    case "2":
+                        return Difficulty.Medium;
+                    case "3":
+                        return Difficulty.Hard;
+                    default:
+                        Console.WriteLine("Please enter a valid choice!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ csharp-practice-projects/NumberGuessingGame/Round.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class Round
+    {
+        private int number;
+        private int maxNumber;
+        private int attempts;
+
+        public int Attempts => attempts;
+
+        public Round(Difficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    this.maxNumber = 10;
+                    break;
+                case Difficulty.Medium:
+                    this.maxNumber = 50;
+                    break;
+                case Difficulty.Hard:
+                    this.maxNumber = 100;
+                    break;
+                default:
+                    maxNumber = 10;
+                    break;
+            }
+
+            this.number = new Random().Next(1, this.maxNumber + 1);
+            this.attempts = 0;
+        }
+
+        public void Play()
+        {
+            int guess = 0;
+
+            while(guess != this.number)
+            {
+                Console.Write("\nenter your guess: ");
+                string input = Console.ReadLine();
+
+                bool isValid = int.TryParse(input, out guess);
+
+                if (!isValid || guess < 1 || guess > this.maxNumber)
+                {
+                    Console.WriteLine($"Please enter a valid number between 1 and {this.maxNumber}");
+                    continue;
+                }
+
+                this.attempts++;
+
+                if (guess < this.number)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (guess > this.number)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is {this.number}! You guessed it in {this.attempts} attempts");
+                }
+            }
+        }
+    }
+}
